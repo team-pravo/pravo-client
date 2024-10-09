0f5cbf4fce47aa686804b8765ab21ddba25affb2
gaeguul <jeeham0226@gmail.com>
2024-10-09 17:19:29 +0900

    feat: HomeScreen 구현 및 상세페이지 연결

--- lib/features/promise_list/presentation/screens/promise_list_screen.dart
+++ lib/features/promise_list/presentation/screens/promise_list_screen.dart
@@ -1,20 +1,12 @@
 // 약속 목록 페이지
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pravo_client/features/auth/presentation/viewmodels/user_provider.dart';
 
-class PromiseListScreen extends ConsumerWidget {
+class PromiseListScreen extends StatelessWidget {
   const PromiseListScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(userProvider);
-
-    return Scaffold(
-      body: Center(
-        child: Text("로그인 상태: ${user?.name}"),
-      ),
-    );
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
