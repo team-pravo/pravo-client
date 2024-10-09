e66fdf7a218b32d3b1736cd427ae0d94997efc0b
Nayeon Kim <inourbubble2@gmail.com>
2024-10-09 15:52:07 +0900

    feat: Depth1 & Depth2 AppBar 컴포넌트 구현

---
+++ lib/app/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pravo_client/assets/constants.dart' as constants;
+
+final ThemeData appTheme = ThemeData(
+  appBarTheme: const AppBarTheme(
+    titleTextStyle: TextStyle(
+      fontSize: constants.kAppBarFontSize,
+      fontWeight: constants.kAppBarFontWeight,
+      color: Colors.black,
+    ),
+    iconTheme: IconThemeData(
+      size: constants.kAppBarIconSize,
+      color: Colors.black,
+    ),
+  ),
+);
---
+++ lib/assets/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// AppBar
+const double kAppBarHeight = 50;
+const double kAppBarFontSize = 16;
+const FontWeight kAppBarFontWeight = FontWeight.w500;
+const double kAppBarIconSize = 24;
