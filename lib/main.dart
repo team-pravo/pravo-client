2c27958321a1881a785bfddc372ddefb101f0df2
Nayeon Kim <inourbubble2@gmail.com>
2024-10-09 15:52:07 +0900

    feat: Depth1 & Depth2 AppBar 컴포넌트 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pravo_client/app/router.dart';
+import 'package:pravo_client/app/theme.dart';
 
 void main() {
   runApp(_App());
@@ -11,6 +12,7 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      theme: appTheme,
       routerConfig: PravoRouter.router,
       theme: ThemeData(
         scaffoldBackgroundColor: backgroundColor,
