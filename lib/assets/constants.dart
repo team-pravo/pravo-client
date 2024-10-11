4cf758e50a5bc4899883efe1bc1968bd9b3eb686
gaeguul <jeeham0226@gmail.com>
2024-10-11 12:07:38 +0900

    feat: LoginScreen 구현

--- lib/assets/constants.dart
+++ lib/assets/constants.dart
@@ -9,3 +9,4 @@ const double kAppBarIconSize = 24;
 // Colors
 const Color kPrimaryColor = Color(0xFF74A739);
 const Color kBackgroundColor = Color.fromRGBO(246, 246, 246, 1);
+const Color kPrimaryColor = Color(0xFF74A739);
