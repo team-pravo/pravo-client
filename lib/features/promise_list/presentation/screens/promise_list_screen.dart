2620bf04cfcca86a704dab864ce3fd39150dea8d
gaeguul <jeeham0226@gmail.com>
2024-10-16 16:07:34 +0900

    feat: LoginScreen 및 소셜 로그인 위젯 구현

--- lib/features/promise_list/presentation/screens/promise_list_screen.dart
+++ lib/features/promise_list/presentation/screens/promise_list_screen.dart
@@ -1,12 +1,20 @@
 // 약속 목록 페이지
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pravo_client/features/auth/presentation/viewmodels/user_provider.dart';
 
 class PromiseListScreen extends StatelessWidget {
   const PromiseListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider);
+
+    return Scaffold(
+      body: Center(
+        child: Text("로그인 상태: ${user?.name}"),
+      ),
+    );
   }
 }
