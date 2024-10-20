ee0386c1c35199e500043f83bbf9fb65de83d5b9
Nayeon Kim <inourbubble2@gmail.com>
2024-10-20 17:10:23 +0900

    feat: 약속 목록 페이지 뼈대 구현

--- lib/features/promise_list/presentation/screens/promise_list_screen.dart
+++ lib/features/promise_list/presentation/screens/promise_list_screen.dart
@@ -1,12 +1,22 @@
 // 약속 목록 페이지
 
 import 'package:flutter/material.dart';
+import 'package:pravo_client/features/core/presentation/widgets/depth1_app_bar_widget.dart';
+import 'package:pravo_client/features/core/presentation/widgets/navigation_bar_widget.dart';
 
 class PromiseListScreen extends StatelessWidget {
   const PromiseListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: Depth1AppBarWidget(
+        title: "약속 목록",
+        actionIcon: Icons.notifications_none_rounded,
+        actionOnPressed: () {},
+      ),
+      body: const Center(),
+      bottomNavigationBar: const NavigationBarWidget(),
+    );
   }
 }
