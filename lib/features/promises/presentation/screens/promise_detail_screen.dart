5ba35c9091efd7702e6b2a0d2b14b3b80ceb5d63
Nayeon Kim <inourbubble2@gmail.com>
2024-10-27 21:24:05 +0900

    feat: 약속 상세 하단 버튼 추가

--- lib/features/promises/presentation/screens/promise_detail_screen.dart
+++ lib/features/promises/presentation/screens/promise_detail_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth2_app_bar_widget.dart';
+import 'package:pravo_client/features/core/presentation/widgets/primary_button_widget.dart';
 import 'package:pravo_client/features/promises/presentation/widgets/deposit_widget.dart';
 import 'package:pravo_client/features/promises/presentation/widgets/participants_widget.dart';
 import 'package:pravo_client/features/promises/presentation/widgets/promise_overview_widget.dart';
@@ -19,14 +21,25 @@ class PromiseDetailScreen extends ConsumerWidget {
         actionIcon: Icons.delete_outline,
         actionOnPressed: () => {},
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(27, 50, 38, 0),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(27, 50, 38, 37),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            PromiseOverviewWidget(),
-            ParticipantsWidget(),
-            DepositWidget(),
+            const PromiseOverviewWidget(),
+            const ParticipantsWidget(),
+            const DepositWidget(),
+            const Spacer(
+              flex: 1,
+            ),
+            PrimaryButtonWidget(
+              onTap: () {},
+              buttonColor: kPrimaryColor,
+              textColor: Colors.white,
+              buttonText: '초대 링크 복사',
+              icon: Icons.ios_share,
+              iconBeforeText: true,
+            ),
           ],
         ),
       ),
