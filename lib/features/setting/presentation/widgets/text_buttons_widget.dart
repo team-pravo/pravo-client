c8033b4cdf952342ab23130b070ca05d05221b76
gaeguul <jeeham0226@gmail.com>
2024-10-27 15:23:09 +0900

    refactor: TextButtonsWidget에 로그아웃 기능 추가 및 임시 로그아웃 버튼 제거

--- lib/features/setting/presentation/widgets/text_buttons_widget.dart
+++ lib/features/setting/presentation/widgets/text_buttons_widget.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:pravo_client/features/auth/presentation/viewmodels/auth_provider.dart';
 import 'package:pravo_client/features/setting/presentation/widgets/text_button_widget.dart';
 
-class TextButtonsWidget extends StatelessWidget {
+class TextButtonsWidget extends ConsumerWidget {
   const TextButtonsWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 카카오톡 로그아웃
+    Future<void> logout() async {
+      final authNotifier = ref.read(authProvider);
+
+      try {
+        await UserApi.instance.logout();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('로그아웃 성공, SDK에서 토큰 삭제')),
+        );
+        await authNotifier.logout(); // 로그아웃 시 AuthNotifier의 상태 업데이트
+      } catch (error) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('카카오톡으로 로그인 실패 $error')));
+      }
+    }
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -31,7 +50,7 @@ class TextButtonsWidget extends StatelessWidget {
         ),
         TextButtonWidget(
           title: '로그아웃',
-          actionOnTap: () => {},
+          actionOnTap: logout,
         ),
         TextButtonWidget(
           title: '탈퇴하기',
