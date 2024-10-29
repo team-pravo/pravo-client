e4f8c415fddf9684ecbbcfe55e6b7495ae41ba2a
gaeguul <jeeham0226@gmail.com>
2024-10-29 14:10:10 +0900

    refactor: Home 스크린 리팩토링

---
+++ lib/features/home/presentation/widgets/today_promises_overview_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pravo_client/features/core/presentation/widgets/promise_widget.dart';
+
+class TodayPromisesOverviewWidget extends StatelessWidget {
+  const TodayPromisesOverviewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '오늘의 약속',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        // 약속 위젯
+        PromiseWidget(),
+      ],
+    );
+  }
+}
