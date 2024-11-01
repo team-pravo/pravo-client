2366a58c124ee3f6c8c75185c7174e4bbe80faae
gaeguul <jeeham0226@gmail.com>
2024-11-01 10:15:50 +0900

    refactor: Credentials 명칭 사용 및 저장소에 platform 저장하도록 구현

--- lib/features/auth/presentation/viewmodels/auth_provider.dart
+++ lib/features/auth/presentation/viewmodels/auth_provider.dart
@@ -9,45 +9,53 @@ final authProvider =
 class AuthNotifier extends ChangeNotifier {
   final Ref ref;
   final _secureStorage = const FlutterSecureStorage();
-  bool _isTokenRead = false;
+  bool _isCredentialsLoaded = false;
   String? _token;
   Platform? _platform;
 
-  bool get isTokenRead => _isTokenRead;
-
   AuthNotifier({required this.ref}) {
-    _readTokenFromStorage(); // 초기화 시 토큰 확인
+    _loadCredentialsFromStorage(); // 초기화 시 토큰 확인
   }
 
   Platform? getPlatform() {
     return _platform;
   }
 
-  // 저장소에 저장된 토큰 확인
-  Future<void> _readTokenFromStorage() async {
+  /// 저장소에 저장된 정보 확인
+  Future<void> _loadCredentialsFromStorage() async {
     _token = await _secureStorage.read(key: 'token');
-    _isTokenRead = true;
+
+    String? platform = await _secureStorage.read(key: 'platform');
+    if (platform == Platform.apple.name) {
+      _platform = Platform.apple;
+    } else if (platform == Platform.kakao.name) {
+      _platform = Platform.kakao;
+    }
+
+    _isCredentialsLoaded = true;
     notifyListeners();
   }
 
   Future<bool> isTokenValid() async {
-    if (!_isTokenRead) {
-      await _readTokenFromStorage();
+    if (!_isCredentialsLoaded) {
+      await _loadCredentialsFromStorage();
     }
     return _token != null;
   }
 
-  // 토큰 저장 로직
+  /// 로그인
   Future<void> login(String newToken, Platform newPlatform) async {
     await _secureStorage.write(key: 'token', value: newToken);
+    await _secureStorage.write(key: 'platform', value: newPlatform.name);
     _token = newToken;
     _platform = newPlatform;
     notifyListeners();
   }
 
-  // 토큰 삭제 로직
+  /// 로그아웃
   Future<void> logout() async {
     await _secureStorage.delete(key: 'token');
+    await _secureStorage.delete(key: 'platform');
     _token = null;
     _platform = null;
     notifyListeners();
