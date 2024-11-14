693165958795b363bdb2d3aa23c1bf49df19071a
gaeguul <jeeham0226@gmail.com>
2024-11-14 17:39:09 +0900

    style: 아이콘 수정

--- lib/features/promises/presentation/screens/promises_screen.dart
+++ lib/features/promises/presentation/screens/promises_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth1_app_bar_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/navigation_bar_widget.dart';
@@ -30,7 +31,7 @@ class PromisesScreen extends ConsumerWidget {
       child: Scaffold(
         appBar: Depth1AppBarWidget(
           title: '약속 목록',
-          actionIcon: Icons.notifications,
+          actionIcon: PhosphorIcons.bell(),
           actionOnPressed: () {},
         ),
         body: Column(
