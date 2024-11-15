bc3f5abfe1d5c98a027d87336fbfdc9ee333c557
gaeguul <jeeham0226@gmail.com>
2024-11-15 12:01:42 +0900

    refactor: 주문 API 요청 실패 시 에러 화면 이동 대신 재시도 모달 추가

--- lib/features/new/presentation/widgets/go_to_desposit_screen_button_widget.dart
+++ lib/features/new/presentation/widgets/go_to_desposit_screen_button_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pravo_client/assets/constants.dart';
+import 'package:pravo_client/features/core/presentation/widgets/alert_dialog_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/primary_button_widget.dart';
 import 'package:pravo_client/features/new/domain/entities/payment.dart';
 import 'package:pravo_client/features/new/presentation/viewmodels/payment_view_model.dart';
@@ -29,7 +30,18 @@ class GoToDepositScreenButtonWidget extends ConsumerWidget {
           context.push('/new/deposit');
         },
         error: (error, stackTrace) {
-          context.push('/new/error');
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialogWidget(
+              title: '잠시후 다시 시도해주세요.',
+              content: '네트워크 연결이 불안정하거나\n서버 요청에 실패했습니다.',
+              actionOnPressed: () {
+                Navigator.of(context).pop();
+                ref.read(paymentViewModelProvider.notifier).fetchPayment();
+              },
+              actionTitle: '다시 시도',
+            ),
+          );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
       );
