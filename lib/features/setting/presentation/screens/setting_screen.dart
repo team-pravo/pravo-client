83110b55b25843de6ab442c539ea6972a3f31c70
Nayeon Kim <inourbubble2@gmail.com>
2024-11-20 19:47:17 +0900

    feat: 설정 진입 시 Member 조회하도록 구현

--- lib/features/setting/presentation/screens/setting_screen.dart
+++ lib/features/setting/presentation/screens/setting_screen.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth1_app_bar_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/navigation_bar_widget.dart';
+import 'package:pravo_client/features/setting/presentation/viewmodels/member_view_model.dart';
 import 'package:pravo_client/features/setting/presentation/widgets/profile_preview_widget.dart';
 import 'package:pravo_client/features/setting/presentation/widgets/text_buttons_widget.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends ConsumerStatefulWidget {
   const SettingScreen({super.key});
 
   @override
+  ConsumerState<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends ConsumerState<SettingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ref.read(memberViewModelProvider.notifier).getMember();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final memberState = ref.watch(memberViewModelProvider);
+
     return Scaffold(
       appBar: Depth1AppBarWidget(
         title: '설정',
@@ -21,12 +38,20 @@ class SettingScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Container(
             padding: kScreenPadding,
-            child: const Column(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ProfilePreviewWidget(),
-                SizedBox(height: 50),
-                TextButtonsWidget(),
+                memberState.when(
+                  data: (member) => ProfilePreviewWidget(
+                    name: member.name,
+                    profileImageUrl: member.profileImageUrl,
+                  ),
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                  error: (err, stack) => Center(child: Text('Error: $err')),
+                ),
+                const SizedBox(height: 50),
+                const TextButtonsWidget(),
               ],
             ),
           ),
