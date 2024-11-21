658abaaa4db3cd33d8582b7a6bb9273660423dc3
gaeguul <jeeham0226@gmail.com>
2024-11-21 18:55:49 +0900

    feat: 약속 정보와 POST 요청 엔티티 분리

--- lib/features/new/domain/entities/payment_request.dart
+++ lib/features/new/domain/entities/payment_request.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-
 class PaymentRequest {
   final String name;
   final String location;
@@ -25,41 +22,6 @@ class PaymentRequest {
       location: location ?? this.location,
       deposit: deposit ?? this.deposit,
       scheduledAt: scheduledAt ?? this.scheduledAt,
-    );
-  }
-
-  String get formattedDate {
-    return DateFormat('MM월 dd일 EEEE', 'ko_KR').format(scheduledAt);
-  }
-
-  String formattedTime(BuildContext context) {
-    return TimeOfDay(
-      hour: scheduledAt.hour,
-      minute: scheduledAt.minute,
-    ).format(context);
-  }
-
-  PaymentRequest updateDate(DateTime newDate) {
-    return copyWith(
-      scheduledAt: DateTime(
-        newDate.year,
-        newDate.month,
-        newDate.day,
-        scheduledAt.hour,
-        scheduledAt.minute,
-      ),
-    );
-  }
-
-  PaymentRequest updateTime(TimeOfDay newTime) {
-    return copyWith(
-      scheduledAt: DateTime(
-        scheduledAt.year,
-        scheduledAt.month,
-        scheduledAt.day,
-        newTime.hour,
-        newTime.minute,
-      ),
     );
   }
 }
