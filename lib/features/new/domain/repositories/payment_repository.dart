171391878bbc73de25dd272f7ce606f98ee6cd80
gaeguul <jeeham0226@gmail.com>
2024-11-21 12:57:17 +0900

    feat: 결제 요청 usecase 생성 및 repository 생성

--- lib/features/new/domain/repositories/payment_repository.dart
+++ lib/features/new/domain/repositories/payment_repository.dart
@@ -1,5 +1,6 @@
-import 'package:pravo_client/features/new/domain/entities/payment.dart';
+import 'package:pravo_client/features/new/domain/entities/payment_request.dart';
+import 'package:pravo_client/features/new/domain/entities/payment_response.dart';
 
 abstract class PaymentRepository {
-  Future<Payment> getPayment();
+  Future<PaymentResponse> requestPayment(PaymentRequest request);
 }
