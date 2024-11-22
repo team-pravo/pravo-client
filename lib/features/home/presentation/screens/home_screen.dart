e3a141b3f6592de1369538a09e4fd96d9965366e
gaeguul <jeeham0226@gmail.com>
2024-11-22 12:09:25 +0900

    temp: 알림 스크린 테스트를 위한 url 연결

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth1_app_bar_widget.dart';
@@ -17,7 +18,9 @@ class HomeScreen extends ConsumerWidget {
       appBar: Depth1AppBarWidget(
         title: '홈',
         actionIcon: PhosphorIcons.bell(),
-        actionOnPressed: () {},
+        actionOnPressed: () {
+          context.push('/hello'); //FIXME: 테스트를 위한 코드이므로 추후 삭제 예정
+        },
       ),
       body: const SingleChildScrollView(
         child: Padding(
