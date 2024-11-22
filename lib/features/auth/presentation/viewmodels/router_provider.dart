c4a8440af725f875e17913a263eef70fe143e8c3
gaeguul <jeeham0226@gmail.com>
2024-11-22 12:06:18 +0900

    feat: 존재하지 않는 URL, '/error' 에 에러 스크린 연결

--- lib/features/auth/presentation/viewmodels/router_provider.dart
+++ lib/features/auth/presentation/viewmodels/router_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pravo_client/features/auth/presentation/screens/login_screen.dart';
 import 'package:pravo_client/features/auth/presentation/viewmodels/auth_provider.dart';
+import 'package:pravo_client/features/error/presentation/screens/error_screen.dart';
 import 'package:pravo_client/features/home/presentation/screens/home_screen.dart';
 import 'package:pravo_client/features/new/presentation/screens/deposit_payment_complete_screen.dart';
 import 'package:pravo_client/features/new/presentation/screens/deposit_payment_screen.dart';
@@ -86,6 +87,17 @@ final routerProvider = Provider<GoRouter>((ref) {
       path: '/profile/edit',
       builder: (_, __) => const ProfileEditScreen(),
     ),
+    GoRoute(
+      path: '/error',
+      builder: (_, state) {
+        final data = state.extra! as Map<String, dynamic>;
+        return ErrorScreen(
+          appBarTitle: data['appBarTitle'],
+          errorTitle: data['errorTitle'],
+          errorMessage: data['errorMessage'],
+        );
+      },
+    ),
   ];
 
   // 로그인 상태에 따른 리디렉션 로직
@@ -106,9 +118,14 @@ final routerProvider = Provider<GoRouter>((ref) {
 
   return GoRouter(
     routes: routes,
-    redirect: (_, state) =>
-        redirectLogic(authNotifier, state), // navigate될 때마다 호출
-    refreshListenable:
-        authNotifier, // authProvider 상태를 listen하여 상태가 변하면 redirect 실행
+    redirect: (_, state) => redirectLogic(authNotifier, state),
+    refreshListenable: authNotifier,
+    errorBuilder: (context, state) {
+      return const ErrorScreen(
+        appBarTitle: '페이지 오류',
+        errorTitle: '찾을 수 없는 페이지',
+        errorMessage: '요청하신 페이지가 존재하지 않습니다.',
+      );
+    },
   );
 });
