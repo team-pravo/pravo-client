b77c13addb285a7833cf9f7394448febeb460b67
Nayeon Kim <inourbubble2@gmail.com>
2024-11-22 12:03:52 +0900

    refactor: ButtonStatus 분리

--- lib/features/promise/presentation/screens/promise_detail_screen.dart
+++ lib/features/promise/presentation/screens/promise_detail_screen.dart
@@ -7,17 +7,12 @@ import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/alert_dialog_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth2_app_bar_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/primary_button_widget.dart';
+import 'package:pravo_client/features/promise/domain/entities/button_status.dart';
 import 'package:pravo_client/features/promise/presentation/viewmodels/promise_view_model.dart';
 import 'package:pravo_client/features/promise/presentation/widgets/deposit_widget.dart';
 import 'package:pravo_client/features/promise/presentation/widgets/participants_and_status_widget.dart';
 import 'package:pravo_client/features/promise/presentation/widgets/promise_overview_widget.dart';
 import 'package:pravo_client/features/promise/presentation/widgets/promise_status_widget.dart';
-
-enum ButtonState {
-  copyInvitationLink, // 초대 링크 복사 상태
-  goToAttendanceConfirmation, // 참석 확인하러 가기 상태
-  hidden, // 버튼 숨김 상태
-}
 
 class PromiseDetailScreen extends ConsumerStatefulWidget {
   final int promiseId;
@@ -28,19 +23,6 @@ class PromiseDetailScreen extends ConsumerStatefulWidget {
     required this.promiseId,
     this.isAttendanceConfirmed = false,
   });
-
-  ButtonState getButtonState(bool isOrganizer, DateTime scheduledAt) {
-    final currentTime = DateTime.now();
-    final timeDifferenceInMinutes =
-        scheduledAt.difference(currentTime).inMinutes;
-
-    if (isOrganizer) {
-      return timeDifferenceInMinutes <= 60
-          ? ButtonState.goToAttendanceConfirmation
-          : ButtonState.copyInvitationLink;
-    }
-    return ButtonState.hidden;
-  }
 
   @override
   ConsumerState<PromiseDetailScreen> createState() =>
@@ -64,8 +46,8 @@ class _PromiseDetailScreenState extends ConsumerState<PromiseDetailScreen> {
       data: (state) {
         final promise = state.promise;
         final isOrganizer = state.isOrganizer;
-        final buttonState =
-            widget.getButtonState(isOrganizer, promise.promiseDate);
+        final buttonStatus =
+            ButtonStatus.getButtonStatus(isOrganizer, promise.promiseDate);
 
         return Scaffold(
           appBar: Depth2AppBarWidget(
@@ -128,17 +110,17 @@ class _PromiseDetailScreenState extends ConsumerState<PromiseDetailScreen> {
                     ),
                   ),
                 ),
-                if (buttonState != ButtonState.hidden)
+                if (buttonStatus != ButtonStatus.hidden)
                   PrimaryButtonWidget(
                     isEnabled: true,
                     onTap: () {
-                      if (buttonState == ButtonState.copyInvitationLink) {
+                      if (buttonStatus == ButtonStatus.copyInvitationLink) {
                         Clipboard.setData(const ClipboardData(text: '초대 링크'));
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(content: Text('초대 링크가 복사되었습니다.')),
                         );
-                      } else if (buttonState ==
-                          ButtonState.goToAttendanceConfirmation) {
+                      } else if (buttonStatus ==
+                          ButtonStatus.goToAttendanceConfirmation) {
                         context.push(
                           '/promise/${widget.promiseId}/confirm-attendance',
                         );
@@ -146,10 +128,8 @@ class _PromiseDetailScreenState extends ConsumerState<PromiseDetailScreen> {
                     },
                     buttonColor: kPrimaryColor,
                     textColor: Colors.white,
-                    buttonText: buttonState == ButtonState.copyInvitationLink
-                        ? '초대 링크 복사'
-                        : '참석 확인하러 가기',
-                    icon: buttonState == ButtonState.copyInvitationLink
+                    buttonText: buttonStatus.text,
+                    icon: buttonStatus == ButtonStatus.copyInvitationLink
                         ? PhosphorIcons.link(PhosphorIconsStyle.bold)
                         : null,
                     iconBeforeText: true,
