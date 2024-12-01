07c1df2a88339d22f1b576425a87f7eb3ef0c6e5
Nayeon Kim <inourbubble2@gmail.com>
2024-12-01 23:12:28 +0900

    feat: 이미지를 1MB 이하로 압축하도록 수정

--- lib/features/member/domain/usecases/edit_member_usecase.dart
+++ lib/features/member/domain/usecases/edit_member_usecase.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:mime/mime.dart';
+import 'package:uuid/uuid.dart';
 import 'package:pravo_client/features/member/domain/repositories/member_repository.dart';
 
 class EditMemberUseCase {
@@ -16,13 +18,17 @@ class EditMemberUseCase {
     required bool resetToDefaultImage,
   }) async {
     if (file != null) {
-      final mimeType = lookupMimeType(file.path) ?? 'application/octet-stream';
+      final compressedFile = await _convertAndCompressToJpeg(file);
+
+      final mimeType =
+          lookupMimeType(compressedFile.path) ?? 'application/octet-stream';
       final mimeTypeSplit = mimeType.split('/');
       final profileImageFile = await MultipartFile.fromFile(
-        file.path,
-        filename: file.path.split('/').last,
+        compressedFile.path,
+        filename: compressedFile.path.split('/').last,
         contentType: MediaType(mimeTypeSplit[0], mimeTypeSplit[1]),
       );
+
       return repository.editMember(
         name: name,
         file: profileImageFile,
@@ -34,5 +40,26 @@ class EditMemberUseCase {
       name: name,
       resetToDefaultImage: resetToDefaultImage,
     );
+  }
+
+  Future<File> _convertAndCompressToJpeg(File file) async {
+    const uuid = Uuid();
+    File compressedFile = file;
+    int quality = 85;
+
+    do {
+      final targetPath = '${file.parent.path}/compressed_${uuid.v4()}.jpg';
+      final compressedXFile = await FlutterImageCompress.compressAndGetFile(
+        compressedFile.path,
+        targetPath,
+        format: CompressFormat.jpeg,
+        quality: quality,
+      );
+      compressedFile = File(compressedXFile!.path);
+      quality -= 5;
+    } while (compressedFile.lengthSync() >
+        1024 * 1024); // FIXME: 추후 서버 nginx 문제가 해결되면 1MB보다 큰 값으로 수정 필요함
+
+    return compressedFile;
   }
 }
