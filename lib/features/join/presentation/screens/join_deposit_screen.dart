754a8401d10aeedd00f465f4fc8d37e1e837c655
gaeguul <jeeham0226@gmail.com>
2024-12-05 20:56:33 +0900

    feat: paymentKey get한 뒤에 confirmPaymentAndChangeParticipantStatus 로직 수행

--- lib/features/join/presentation/screens/join_deposit_screen.dart
+++ lib/features/join/presentation/screens/join_deposit_screen.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:pravo_client/app/formatter.dart';
+import 'package:pravo_client/features/auth/presentation/viewmodels/router_provider.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth2_app_bar_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/primary_button_widget.dart';
+import 'package:pravo_client/features/join/presentation/viewmodels/join_promise_view_model.dart';
+import 'package:pravo_client/features/join/presentation/viewmodels/join_view_model.dart';
+import 'package:pravo_client/features/join/presentation/viewmodels/payment_response_provider.dart';
 import 'package:pravo_client/features/payment/presentation/viewmodels/deposit_payment_view_model.dart';
 import 'package:tosspayments_widget_sdk_flutter/widgets/agreement.dart';
 import 'package:tosspayments_widget_sdk_flutter/widgets/payment_method.dart';
@@ -21,10 +26,12 @@ class _JoinDepositScreenState extends ConsumerState<JoinDepositScreen> {
     final paymentState = ref.watch(depositPaymentViewModelProvider);
     final paymentNotifier = ref.read(depositPaymentViewModelProvider.notifier);
 
-    const promiseId = 'promiseId';
-    const promiseName = 'promiseName';
-    const depositAmount = '1,000';
-    const orderId = 'orderId';
+    final joinPromise = ref.watch(joinPromiseProvider)!;
+    final promiseId = joinPromise.id;
+    final promiseName = joinPromise.name;
+    final formattedDepositAmount =
+        Formatter.formatWithComma(joinPromise.deposit);
+    final orderId = ref.watch(paymentResponseProvider)!.orderId;
 
     return Scaffold(
       appBar: Depth2AppBarWidget(
@@ -58,10 +65,21 @@ class _JoinDepositScreenState extends ConsumerState<JoinDepositScreen> {
               ),
             ),
             PrimaryButtonWidget(
-              buttonText: '$depositAmount원 결제하기',
+              buttonText: '$formattedDepositAmount원 결제하기',
               isEnabled: paymentState.isAgreementChecked,
               onTap: () async {
-                await paymentNotifier.getPaymentKey(orderId, promiseName);
+                final paymentKey =
+                    await paymentNotifier.getPaymentKey(orderId, promiseName);
+
+                if (paymentKey != null) {
+                  await ref
+                      .read(joinViewModelProvider.notifier)
+                      .confirmPaymentAndChangeParticipantStatus(paymentKey);
+
+                  ref
+                      .read(routerProvider)
+                      .pushReplacement('/promise/$promiseId/join/complete');
+                }
               },
               hasHorizontalMargin: true,
             ),
