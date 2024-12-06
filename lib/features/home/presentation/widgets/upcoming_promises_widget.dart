ec39d59e3b9efb7ba11a6e5a23977be8c3fd131b
gaeguul <jeeham0226@gmail.com>
2024-12-07 00:03:30 +0900

    style: 다가오는 약속 사이에 gap 추가

--- lib/features/home/presentation/widgets/upcoming_promises_widget.dart
+++ lib/features/home/presentation/widgets/upcoming_promises_widget.dart
@@ -20,28 +20,64 @@ class UpcomingPromisesWidget extends StatelessWidget {
         borderRadius: kWidgetBorderRadius,
       ),
       width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            upcomingPromises.isEmpty ? '이번 주 다가오는 약속이 없어요.' : '다가오는 약속',
-            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-          ),
-          if (upcomingPromises.isNotEmpty) const SizedBox(height: 16),
-          ...upcomingPromises.map((promise) {
-            return Row(
+      child: upcomingPromises.isEmpty
+          ? const Text(
+              '이번 주 다가오는 약속이 없어요.',
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  Formatter.formatWeekDay(promise.scheduledAt), // 요일
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+                const Text(
+                  '다가오는 약속',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
                 ),
-                const SizedBox(width: 24),
-                Text(promise.name), // 약속 이름
+                const SizedBox(height: 16),
+                ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: upcomingPromises.length,
+                  separatorBuilder: (context, index) =>
+                      const SizedBox(height: 8),
+                  itemBuilder: (context, index) {
+                    final promise = upcomingPromises[index];
+                    return Row(
+                      children: [
+                        Text(
+                          Formatter.formatWeekDay(promise.scheduledAt), // 요일
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(width: 24),
+                        Text(promise.name), // 약속 이름
+                      ],
+                    );
+                  },
+                ),
               ],
-            );
-          }),
-        ],
-      ),
+            ),
+
+      // child: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     Text(
+      //       upcomingPromises.isEmpty ? '이번 주 다가오는 약속이 없어요.' : '다가오는 약속',
+      //       style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+      //     ),
+      //     if (upcomingPromises.isNotEmpty) const SizedBox(height: 16),
+      //     ...upcomingPromises.map((promise) {
+      //       return Row(
+      //         children: [
+      //           Text(
+      //             Formatter.formatWeekDay(promise.scheduledAt), // 요일
+      //             style: const TextStyle(fontWeight: FontWeight.bold),
+      //           ),
+      //           const SizedBox(width: 24),
+      //           Text(promise.name), // 약속 이름
+      //         ],
+      //       );
+      //     }),
+      //   ],
+      // ),
     );
   }
 }
