4249d4a96b9cddf9d5521a02d298a6132f14679d
gaeguul <jeeham0226@gmail.com>
2024-12-06 23:25:17 +0900

    feat: 정산 버튼 클릭 시 API 요청 기능 및 에러 처리 구현

--- lib/features/settlement/presentation/screens/confirm_attendance_screen.dart
+++ lib/features/settlement/presentation/screens/confirm_attendance_screen.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pravo_client/assets/constants.dart';
 import 'package:pravo_client/features/core/presentation/widgets/depth2_app_bar_widget.dart';
 import 'package:pravo_client/features/core/presentation/widgets/primary_button_widget.dart';
+import 'package:pravo_client/features/settlement/domain/entites/settlement.dart';
+import 'package:pravo_client/features/settlement/presentation/viewmodels/selected_attendees_view_model.dart';
+import 'package:pravo_client/features/settlement/presentation/viewmodels/settlement_view_model.dart';
 import 'package:pravo_client/features/settlement/presentation/widgets/attendance_list_widget.dart';
 
-class ConfirmAttendanceScreen extends StatelessWidget {
+class ConfirmAttendanceScreen extends ConsumerWidget {
   final int promiseId;
 
   const ConfirmAttendanceScreen({super.key, required this.promiseId});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settlementViewModel = ref.read(settlementViewModelProvider.notifier);
+    final selectedAttendees = ref.watch(selectedAttendeesProvider);
+
+    ref.listen<AsyncValue<Settlement>>(settlementViewModelProvider,
+        (previous, next) {
+      next.whenOrNull(
+        data: (settlement) {
+          context.push('/promise/$promiseId/settlement/complete');
+        },
+        error: (error, stack) {
+          context.go(
+            '/error',
+            extra: {
+              'appBarTitle': '오류',
+              'errorTitle': '정산 실패',
+              'errorMessage': '정산 처리 중 문제가 발생했어요. 다시 시도해주세요.',
+            },
+          );
+        },
+      );
+    });
+
     return Scaffold(
       appBar: Depth2AppBarWidget(
         title: '참석 확인',
@@ -47,8 +73,12 @@ class ConfirmAttendanceScreen extends StatelessWidget {
             ),
             PrimaryButtonWidget(
               buttonText: '정산하러 가기',
-              onTap: () =>
-                  context.push('/promise/$promiseId/settlement/complete'),
+              onTap: () {
+                settlementViewModel.confirmSettlement(
+                  attendees: selectedAttendees,
+                  promiseId: promiseId,
+                );
+              },
               hasHorizontalMargin: true,
             ),
           ],
